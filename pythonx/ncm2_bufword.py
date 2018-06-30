93013ee5756255c4d1682675b868daa0e10fefa3
roxma <roxma@qq.com>
2018-06-30 23:18:59 +0800

    filter duplicate words

--- pythonx/ncm2_bufword.py
+++ pythonx/ncm2_bufword.py
@@ -4,6 +4,7 @@ import vim
 from ncm2 import Ncm2Source
 import re
 
+
 class Source(Ncm2Source):
 
     def on_complete(self, ctx):
@@ -23,9 +24,11 @@ class Source(Ncm2Source):
 
         matcher = self.matcher_get(ctx['matcher'])
 
+        seen = {}
+
         step = 1000
         for lidx in range(begin, end, step):
-            lines = buf[lidx : min(lidx + step, end)]
+            lines = buf[lidx: min(lidx + step, end)]
             # convert 0 base to 1 base
             for i, line in enumerate(lines):
                 if lidx + i + 1 == lnum:
@@ -39,15 +42,15 @@ class Source(Ncm2Source):
                             matches.append(m)
                 else:
                     for word in pat.finditer(line):
-                        m = self.match_formalize(ctx, word.group())
-                        if matcher(b, m):
+                        w = word.group()
+                        m = self.match_formalize(ctx, w)
+                        if w not in seen and matcher(b, m):
+                            seen[w] = True
                             matches.append(m)
 
         self.complete(ctx, ctx['startccol'], matches)
 
 
-
 source = Source(vim)
 
 on_complete = source.on_complete
-
